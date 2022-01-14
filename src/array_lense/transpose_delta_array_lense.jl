467eca7b968773153af9412b6354eb4b0ed78276
Ethan Anderes <anderes@ucdavis.edu>
2022-01-13 17:32:57 -0800

    remove tturbo macros to avoid seg faults with M1 Max chip

--- src/array_lense/transpose_delta_array_lense.jl
+++ src/array_lense/transpose_delta_array_lense.jl
@@ -156,9 +156,9 @@ function (τLp::τArrayLensePlan{2,n,Tf,d})(ẏ, t, y) where {n,Tf,d}
 		## @inbounds @. τLp.∇y[2] *= τf 
 		## @turbo @. τLp.w[1] += τLp.mm[1,1] * τLp.∇y[1] + τLp.mm[2,1] * τLp.∇y[2]  
 		## @turbo @. τLp.w[2] += τLp.mm[1,2] * τLp.∇y[1] + τLp.mm[2,2] * τLp.∇y[2] 
-		## --- alt option
-		@tturbo @. τLp.w[1] += τf * (τLp.mm[1,1] * τLp.∇y[1] + τLp.mm[2,1] * τLp.∇y[2])  
-		@tturbo @. τLp.w[2] += τf * (τLp.mm[1,2] * τLp.∇y[1] + τLp.mm[2,2] * τLp.∇y[2]) 
+		## --- alt option (replaced the inbounds with turbo)
+		@inbounds @. τLp.w[1] += τf * (τLp.mm[1,1] * τLp.∇y[1] + τLp.mm[2,1] * τLp.∇y[2])  
+		@inbounds @. τLp.w[2] += τf * (τLp.mm[1,2] * τLp.∇y[1] + τLp.mm[2,2] * τLp.∇y[2]) 
 		## --- alt option
 		# Base.Threads.@threads for ii ∈ eachindex(τLp.w[1])
 		# 	@inbounds τLp.w[1][ii] += τf[ii] * (τLp.mm[1,1][ii] * τLp.∇y[1][ii] + τLp.mm[2,1][ii] * τLp.∇y[2][ii])  
@@ -174,9 +174,9 @@ function (τLp::τArrayLensePlan{2,n,Tf,d})(ẏ, t, y) where {n,Tf,d}
 	∇ⁱvⁱf!(τLp.mm[1,1], τLp.p, τLp.w[1], τLp.∇!, τLp.∇x, τLp.∇y)
 	∇ⁱvⁱf!(τLp.mm[2,1], τLp.p, τLp.w[2], τLp.∇!, τLp.∇x, τLp.∇y)
 	
-	## --- 
-	@tturbo @. ẏ[1] =  - τLp.w[1] - t * τLp.mm[1,1]
-	@tturbo @. ẏ[2] =  - τLp.w[2] - t * τLp.mm[2,1]
+	## --- (replaced the inbounds with turbo)
+	@inbounds @. ẏ[1] =  - τLp.w[1] - t * τLp.mm[1,1]
+	@inbounds @. ẏ[2] =  - τLp.w[2] - t * τLp.mm[2,1]
 	## --- alt option
 	# Base.Threads.@threads for ii ∈ eachindex(ẏ[1])
 	# 	@inbounds ẏ[1][ii] =  - τLp.w[1][ii] - t * τLp.mm[1,1][ii]
